8d60c2f2eba750c3172845cb7a503fe1a3f53326
Ibrahim K <125925656+ibrahim-work@users.noreply.github.com>
2023-10-26 20:25:37 -0400

    Add files via upload

--- player.js
+++ player.js
@@ -1,3 +1,7 @@
+import { Sitting, Running, Jumping, Falling } from './playerStates.js';
+
+
+
 export class Player{
 
     constructor(game){
@@ -5,15 +9,25 @@ export class Player{
         this.width = 100;
         this.height = 91.3;
         this.x = 0;
-        this.y = this.game.height - this.height;
+        this.y = this.game.height - this.height - this.game.groundMargin;
         this.vy = 0;
         this.weight = 1;
         this.image = document.getElementById('player');
+        this.frameX = 0;
+        this.frameY = 0;
+        this.maxFrame;
+        this.fps = 30;
+        this.frameInterval = 1000/this.fps;
+        this.frameTimer = 0;
         this.speed = 0;
         this.maxSpeed = 10; 
+        this.states = [new Sitting(this), new Running(this), new Jumping(this), new Falling(this)];
+        this.currentState = this.states[0];
+        this.currentState.enter();
    
     }
-    update(input){
+    update(input, deltaTime){
+        this.currentState.handleInput(input);
         //horizontal movement
         this.x += this.speed;
         if (input.includes('ArrowRight')) this.speed = this.maxSpeed;
@@ -26,20 +40,38 @@ export class Player{
         //vertical movement
    
        
-        if (input.includes('ArrowUp') && this.onGround()) this.vy -= 20;
         this.y += this.vy;
         if (!this.onGround()) this.vy += this.weight;
         else this.vy = 0;
    
+        //sprite animation
+
+        if (this.frameTimer > this.frameInterval){
+            this.frameTimer = 0;
+        
+            if (this.frameX < this.maxFrame) this.frameX++;
+            else this.frameX = 0;
+
+        } else{
+            this.frameTimer += deltaTime;
+        }
+
+
     }
 
     draw(context){
 
-        context.drawImage(this.image, 0, 0, this.width, this.height, this.x, this.y, this.width, this.height);
+        context.drawImage(this.image, this.frameX * this.width, this.frameY * this.height, this.width, this.height, this.x, this.y, this.width, this.height);
     }
 
     onGround(){
-return this.y >= this.game.height - this.height;
+return this.y >= this.game.height - this.height - this.game.groundMargin;
+    }
+
+    setState(state, speed){
+        this.currentState = this.states[state];
+        this.game.speed = this.game.maxSpeed * speed;
+        this.currentState.enter();
     }
 }
 
